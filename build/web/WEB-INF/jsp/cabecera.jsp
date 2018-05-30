48830d32ec9aafbb7d90a7ff727af5025b9cffce
unknown <dejuanguillermo@gmail.com>
2018-05-30 17:43:20 +0200

    Inicio

---
+++ build/web/WEB-INF/jsp/cabecera.jsp
@@ -0,0 +1,24 @@
+<%-- 
+    Document   : cabecera
+    Created on : 29-may-2018, 17:36:48
+    Author     : usuario
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+        
+    </head>
+    <body>
+        <div style="background-image: url(imagenes/cabecera.jpg);height: 200px;background-size: 100% 100%"/></div>
+        <p align="center">Via Universitas 15, 8º B 50009 ZARAGOZA - rextrella@hotmail.com - Lunes a Viernes de 8:00 a 21:00 </p>
+        </br>        </br>
+        </br>
+        </br>
+        </br>
+
+    </body>
+</html>
---
+++ src/java/modelo/Pelicula.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author usuario
+ */
+public class Pelicula {
+    String nombre, descripcion, lengua, rating;
+    int ano, duracion;
+
+    public Pelicula(String nombre, String descripcion, String lengua, String rating, int ano, int duracion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.lengua = lengua;
+        this.rating = rating;
+        this.ano = ano;
+        this.duracion = duracion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getLengua() {
+        return lengua;
+    }
+
+    public void setLengua(String lengua) {
+        this.lengua = lengua;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+    
+    
+    
+}
